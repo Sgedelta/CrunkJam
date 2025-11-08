b4b67642761b93e796471971d7f6dda7b893655b
matthewfong3 <mxf9125@rit.edu>
2025-11-08 18:32:51 -0500

    lol

---
+++ Assets/Scripts/FeedGame/FeedGameManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class FeedGameManager : MicroGameManager
+{
+    public override void Initialize(InputManager im)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void LoadScene()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UnloadScene()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void BindInput()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
